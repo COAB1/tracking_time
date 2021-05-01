00dfa2dbed2b4c9f68887f3840e8152aa6aaeff9
COAB1 <carol.o.ag@gmail.com>
2021-05-01 15:33:00 -0300

    Include all the buttons on login page

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -30,40 +30,49 @@ class SignInPage extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               )),
           SizedBox(
-            height: 6.0,
-          ),
-          DefaultElevatedButton(
-            child: Text('Sign in with FACEBOOK',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 15.0,
-                )),
-            color: Colors.blue[300],
-            borderRadius: 4.0,
-            onPressed: () {},
-          ),
-          SizedBox(
-            height: 6.0,
-          ),
-          DefaultElevatedButton(
-            child: Text('Sign in with GOOGLE',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 15.0,
-                )),
-            color:  Colors.green[300],
-            borderRadius: 4.0,
-            onPressed: () {},
-          ),
-          SizedBox(
-            height: 6.0,
+            height: 48.0,
           ),
           SignInButton(
-            text: "HOLA",
-            color: Colors.green,
-            textColor: Colors.purple,
-            onPressed: (){},
-          )
+            text: "Sign in with Google",
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: "Sign in with Facebook",
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: "Sign in with email",
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: "Go anonymus",
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
+          ),
         ],
       ),
     );
